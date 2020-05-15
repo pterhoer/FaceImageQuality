d77a8ce84a5c7e3d85c553b0dd7bb2c4a3137ee6
Jan Niklas Kolf <janniklas.kolf@stud.tu-darmstadt.de>
2020-05-15 21:17:13 +0200

    Example code moved to new file; environment.yml added.

--- face_image_quality.py
+++ face_image_quality.py
@@ -4,6 +4,11 @@
 import sys
 from os import path as os_path
 
+import keras.backend as K
+from keras.models import Model as KerasModel
+from keras.layers import Dense, Lambda, Input, Dropout
+from keras.layers.normalization import BatchNormalization
+
 import numpy as np
 import mxnet as mx
 import cv2
@@ -12,11 +17,6 @@ from tqdm import tqdm
 
 from sklearn.preprocessing import normalize
 from sklearn.metrics.pairwise import euclidean_distances
-
-import keras.backend as K
-from keras.models import Model as KerasModel
-from keras.layers import Dense, Lambda, Input, Dropout
-from keras.layers.normalization import BatchNormalization
 
 
 class InsightFace:
@@ -48,7 +48,7 @@ class InsightFace:
         """
 
         sym, arg_params, aux_params = mx.model.load_checkpoint(
-                                        f"{insightface_path}/models/model", 
+                                        f"{insightface_path}models/model", 
                                         0
                                         )
     
@@ -297,38 +297,3 @@ def get_arcface_embedding(img_input, use_preprocessing: bool = True):
     embedding, dropout = insightface_model.get_feature(img_input)
     
     return embedding
-
-if __name__ == "__main__":
-    # Sample code of calculating the embedding and it's score
-    
-    # Create the InsightFace model
-    insightface_model = InsightFace()
-   
-    # Create the SER-FIQ Model
-    ser_fiq = SERFIQ()
-        
-    # Load the test image
-    test_img = cv2.imread("./data/test_img.jpeg")
-    
-    # Calculate the embedding and it's quality score
-    # T=100 (default) is a good choice
-    # Apply preprocessing if image is not aligned (default)
-    embedding, score = get_embedding_quality(test_img,
-                                               insightface_model,
-                                               ser_fiq
-                                               )
-   
-    print("SER-FIQ quality score of image 1 is", score)
-    
-    # Load the test image
-    test_img2 = cv2.imread("./data/test_img2.jpeg")
-    
-    # Calculate the embedding and it's quality score
-    # T=100 is a good choice
-    # Apply preprocessing if image is not aligned
-    embedding2, score2 = get_embedding_quality(test_img2, 
-                                               insightface_model, 
-                                               ser_fiq
-                                               )
-   
-    print("SER-FIQ quality score of image 2 is", score2)
---
+++ serfiq_example.py
@@ -0,0 +1,38 @@
+# Author: Jan Niklas Kolf, 2020
+from face_image_quality import InsightFace, SERFIQ, get_embedding_quality
+import cv2
+
+if __name__ == "__main__":
+    # Sample code of calculating the embedding and it's score
+    
+    # Create the InsightFace model
+    insightface_model = InsightFace()
+   
+    # Create the SER-FIQ Model
+    ser_fiq = SERFIQ()
+        
+    # Load the test image
+    test_img = cv2.imread("./data/test_img.jpeg")
+    
+    # Calculate the embedding and it's quality score
+    # T=100 (default) is a good choice
+    # Apply preprocessing if image is not aligned (default)
+    embedding, score = get_embedding_quality(test_img,
+                                               insightface_model,
+                                               ser_fiq
+                                               )
+   
+    print("SER-FIQ quality score of image 1 is", score)
+    
+    # Load the test image
+    test_img2 = cv2.imread("./data/test_img2.jpeg")
+    
+    # Calculate the embedding and it's quality score
+    # T=100 is a good choice
+    # Apply preprocessing if image is not aligned
+    embedding2, score2 = get_embedding_quality(test_img2, 
+                                               insightface_model, 
+                                               ser_fiq
+                                               )
+   
+    print("SER-FIQ quality score of image 2 is", score2)
